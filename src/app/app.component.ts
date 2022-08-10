56692f3a0da7a37eda6c127b632523baf44d421b
GolovaDanyla <bussinestarget4you@gmail.com>
2022-08-10 17:56:52 +0300

    Finish Cat Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,21 +14,48 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent implements OnInit {
   title = 'catsproject';
   myControl = new FormControl('');
-  options: string[] = ['bang', 'Two', 'Three'];
+  options: string[] = [];
   filteredOptions: Observable<string[]>;
-  userName: string = "";
   response: any;
   breedsCollection: any;
+  foundDataCollection: any;
+  
+  mapOfNames: Map<string, string> = new Map<string, string>([]);
+
   constructor(private http: HttpClient, private cdr: ChangeDetectorRef) {
     this.filteredOptions = new Observable();
 
   }
-  search() {
-    const breed = this.myControl.value;
-    this.http.get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breed}`)
+  onLike(breedId: string, breedName: string) {
+    this.search(breedId, breedName);
+  }
+  search(
+    breedId: string | null | undefined = null,
+    breedName: string | null | undefined = null
+  ) {
+    if(!breedId) {
+      breedName = this.myControl.value as string;
+      if(!breedName) {
+        this.foundDataCollection = null;
+        this.breeds();
+        return;
+      }
+  
+      breedId = this.mapOfNames.get(breedName);
+    }
+    this.http.get<any>(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}&limit=10`)
       .subscribe((response) => {
-        this.response = response;
-        console.log(this.response);
+        ;
+        this.foundDataCollection = response.map((i: any) => {
+          return {
+            id: breedId,
+            name: breedName,
+            url: i.url
+          }
+        });
+        
+        console.log(this.foundDataCollection);
+        
         this.cdr.detectChanges();
       })
   }
@@ -39,24 +66,28 @@ export class AppComponent implements OnInit {
     this.http.get('https://api.thecatapi.com/v1/breeds')
       .subscribe((response: any) => {
         this.response = response.map((i: any) => {
+          this.mapOfNames.set(i.name, i.id);
           return {
+            id: i.id,
             name: i.name,
             url: i?.image?.url
           }
         });
         this.breedsCollection = this.response;
-        this.options = response.map((i:any) => i.id);
+        this.options = response.map((i: any) => i.name);
+        this.filteredOptions = this.myControl.valueChanges.pipe(
+          startWith(''),
+          map(value => this._filter(value || '')),
+        );
         console.log(this.response);
+        console.log('this.mapOfNames', this.mapOfNames);
         this.cdr.detectChanges();
       })
   }
 
   ngOnInit() {
     this.breeds();
-    this.filteredOptions = this.myControl.valueChanges.pipe(
-      startWith(''),
-      map(value => this._filter(value || '')),
-    );
+
   }
 
   private _filter(value: string): string[] {
